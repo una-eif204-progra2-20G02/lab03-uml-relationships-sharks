4ab58beabf1a14a121ab29c200ea9e0f727587d8
John Acuña Gómez <68917346+Jagtec@users.noreply.github.com>
2020-08-16 14:48:25 -0600

    Class Professor

--- Professor.cpp
+++ Professor.cpp
@@ -2,4 +2,54 @@
 // Created by johne on 16/8/2020.
 //
 
+#include <iomanip>
+#include <stdexcept>
+#include <sstream>
 #include "Professor.h"
+
+Professor::Professor():Person(), monthlySalary(0),commissionRate(0){
+
+}
+
+Professor::Professor(string firstname,string lastname, int documentid, int age, double _monthlySalary, double _commissionRate): Person(firstname, lastname, documentid, age), monthlySalary(_monthlySalary), commissionRate(_commissionRate){
+
+}
+
+void Professor::setCommissionRate(double commissionRate){
+    this->commissionRate=commissionRate;
+}
+
+void Professor::setMonthySalary(double monthlySalary){
+    this->monthlySalary=monthlySalary;
+}
+
+double Professor::getMonthySalary(){
+    return monthlySalary;
+}
+
+double Professor::getCommissionRate(){
+    return commissionRate;
+}
+
+double Professor::salary()
+{
+    return monthlySalary;
+}
+
+double Professor::commission()
+{
+    commissionRate = monthlySalary * 0.05;
+    return commissionRate;
+}
+
+string Professor::toString()
+{
+    stringstream s;
+    s<<"Professor Information: "<<firstname<<" "<<lastname<<endl;
+    s<<"Doc Id: "<< documentid <<endl;
+    s<<"Age: "<< age <<endl;
+    s<<"Monthly Salary: " <<fixed<<setprecision(2)<< monthlySalary << endl;
+    s<<"Commission Rate: "<<commission()<<endl;
+
+    return s.str();
+}
--- Professor.h
+++ Professor.h
@@ -4,9 +4,26 @@
 
 #ifndef BASIC_00_EXAMPLE_PROFESSOR_H
 #define BASIC_00_EXAMPLE_PROFESSOR_H
+#include "Person.h"
+#include<iostream>
+#include<sstream>
+using namespace std;
 
+class Professor: public Person{
+private:
+    double monthlySalary;
+    double commissionRate;
+public:
+    Professor();
+    Professor(string , string, int, int, double, double);
+    virtual double getMonthySalary();
+    virtual double getCommissionRate();
+    virtual void setMonthySalary(double);
+    virtual void setCommissionRate(double);
+    double salary() override;
+    string toString()override;
+    double commission();
 
-class Professor {
 
 };
 
