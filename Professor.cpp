bc10bca5f02ce25757d1eabfc44d69cc3eddc0db
John Acuña Gómez <68917346+Jagtec@users.noreply.github.com>
2020-08-16 14:38:30 -0600

    Class Person

---
+++ Professor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by johne on 16/8/2020.
+//
+
+#include "Professor.h"
---
+++ Professor.h
@@ -0,0 +1,14 @@
+//
+// Created by johne on 16/8/2020.
+//
+
+#ifndef BASIC_00_EXAMPLE_PROFESSOR_H
+#define BASIC_00_EXAMPLE_PROFESSOR_H
+
+
+class Professor {
+
+};
+
+
+#endif //BASIC_00_EXAMPLE_PROFESSOR_H
